e7bcc7404d6d1e3986e436ef8a7a145f1fb15f7b
aosdinkim <master@awesomble.com>
2023-01-18 16:30:56 +0900

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import logo from '@/assets/images/logo_white.png'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="logo">
+      <h1><img :src="logo" alt=""></h1>
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  h1 {
+    text-align: center;
+    img {
+      width: 100%;
+      max-width: 150px;
+    }
+  }
+}
+</style>
+
